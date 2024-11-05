1bfbe624613532f29439ce20d13f2ee336009053
Truong <truonghs2903@gmail.com>
2024-11-05 20:01:11 +0700

    Add: Missing files

---
+++ src/utils/constants/sidebar.ts
@@ -0,0 +1,124 @@
+import { icons } from '@/utils/icons'
+
+export const SIDE_BAR_ITEM_LIST = [
+  {
+    title: 'DASHBOARDS',
+    items: [
+      {
+        path: '/',
+        icon: icons.home,
+        title: 'Dashboard'
+      },
+      {
+        path: '/test',
+        icon: icons.home,
+        title: 'Profit'
+      },
+      {
+        path: '/test/test2',
+        icon: icons.home,
+        title: 'Analytics'
+      }
+    ]
+  },
+  {
+    title: 'Products',
+    items: [
+      {
+        path: '/#',
+        icon: icons.home,
+        title: 'All Products'
+      },
+      {
+        path: '/#',
+        icon: icons.home,
+        title: 'Product Detail'
+      },
+      {
+        path: '/#',
+        icon: icons.home,
+        title: 'Edit Product'
+      }
+    ]
+  },
+  {
+    title: 'Products',
+    items: [
+      {
+        path: '/#',
+        icon: icons.home,
+        title: 'All Products'
+      },
+      {
+        path: '/#',
+        icon: icons.home,
+        title: 'Product Detail'
+      },
+      {
+        path: '/#',
+        icon: icons.home,
+        title: 'Edit Product'
+      }
+    ]
+  },
+  {
+    title: 'Products',
+    items: [
+      {
+        path: '/#',
+        icon: icons.home,
+        title: 'All Products'
+      },
+      {
+        path: '/#',
+        icon: icons.home,
+        title: 'Product Detail'
+      },
+      {
+        path: '/#',
+        icon: icons.home,
+        title: 'Edit Product'
+      }
+    ]
+  },
+  {
+    title: 'Products',
+    items: [
+      {
+        path: '/#',
+        icon: icons.home,
+        title: 'All Products'
+      },
+      {
+        path: '/#',
+        icon: icons.home,
+        title: 'Product Detail'
+      },
+      {
+        path: '/#',
+        icon: icons.home,
+        title: 'Edit Product'
+      }
+    ]
+  },
+  {
+    title: 'Products',
+    items: [
+      {
+        path: '/#',
+        icon: icons.home,
+        title: 'All Products'
+      },
+      {
+        path: '/#',
+        icon: icons.home,
+        title: 'Product Detail'
+      },
+      {
+        path: '/#',
+        icon: icons.home,
+        title: 'Edit Product'
+      }
+    ]
+  }
+]
---
+++ src/utils/icons/index.tsx
@@ -0,0 +1,13 @@
+import { FaGear, FaRegUser } from 'react-icons/fa6'
+import { IoHomeOutline, IoSearch } from 'react-icons/io5'
+import { MdMenu } from 'react-icons/md'
+import { RxSlash } from 'react-icons/rx'
+
+export const icons = {
+  menu: <MdMenu />,
+  slash: <RxSlash />,
+  home: <IoHomeOutline />,
+  search: <IoSearch />,
+  setting: <FaGear />,
+  user: <FaRegUser />
+}
