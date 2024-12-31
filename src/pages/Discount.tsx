8afab3af60e40da4cb82314623e32cf6d6fe10b1
QuocDuy03 <quocduyy.03@gmail.com>
2024-12-31 21:45:41 +0700

    add spinner

--- src/pages/Discount.tsx
+++ src/pages/Discount.tsx
@@ -12,6 +12,7 @@ import { IDiscount } from '@/interfaces'
 import { icons } from '@/utils'
 import { useApi, useBoolean } from '@/hooks'
 import { discountApi } from '@/apis'
+import { ProgressSpinner } from 'primereact/progressspinner'
 
 export function Discount() {
   const toast = useRef<Toast>(null)
@@ -33,7 +34,7 @@ export function Discount() {
       const { data } = await discountApi.delete(id)
       if (data) {
         toggleDiscountProductChange()
-        setSelectedDiscountProducts((prevItems) => (prevItems.filter((item) => item.id !== id)))
+        setSelectedDiscountProducts((prevItems) => prevItems.filter((item) => item.id !== id))
         toast.current?.show({ severity: 'success', summary: 'Success', detail: 'Remove Successfully', life: 3000 })
       } else {
         toast.current?.show({ severity: 'error', summary: 'Error', detail: `${errorMessage}`, life: 3000 })
@@ -119,6 +120,18 @@ export function Discount() {
           scrollable
           scrollHeight='638px'
           paginator
+          emptyMessage={
+            loading ? (
+              <div className='flex flex-col items-center justify-center py-10 text-gray-500'>
+                <ProgressSpinner />
+              </div>
+            ) : (
+              <div className='flex flex-col items-center justify-center py-10 text-gray-500'>
+                <p className='text-lg font-semibold'>No Discount Found</p>
+                <p className='text-sm'>Try adjusting your search or filter criteria.</p>
+              </div>
+            )
+          }
           rows={7}
           rowsPerPageOptions={[7, 25, 50]}
           selection={selectedDiscountProducts.length ? selectedDiscountProducts : null}
